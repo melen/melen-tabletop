256331fd12288f68bac455fa5ff383a277bde001
Haowei Gao <hweigao@hotmail.com>
2018-01-12 14:16:19 -0500

    init

---
+++ src/main/java/melentabletop/tabletop/TabletopApplication.java
@@ -0,0 +1,12 @@
+package melentabletop.tabletop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletopApplication.class, args);
+	}
+}
---
+++ src/test/java/melentabletop/tabletop/TabletopApplicationTests.java
@@ -0,0 +1,16 @@
+package melentabletop.tabletop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TabletopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
