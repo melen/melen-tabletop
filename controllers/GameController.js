631c7bda32f2ad85fc08c2e4565a503011bfb140
Haowei Gao <hweigao@hotmail.com>
2018-02-14 17:31:21 -0500

    added rolling and join endpoint

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -5,6 +5,8 @@ let auth = require('../auth/auth');
 
 let gameModel = new Game();
 
+let gameStates = {};
+
 router.get('/create', (req, res) => {
     gameModel .createGame({
         name: "Test User"
@@ -25,6 +27,40 @@ router.put('/:id', auth.jwt, (req, res) => {
 
 router.delete('/:id', auth.jwt, (req, res) => {
     gameModel.deleteGame()
+});
+
+router.post('/roll', (req, res) => {
+    let results = {};
+    for (let dice in req.body) {
+        if (req.body.hasOwnProperty(dice)) {
+            results[dice] = [];
+            for (let i in req.body[dice]) {
+                results[dice].push(Math.floor(Math.random() * Math.floor(dice))+1)
+            }
+        }
+    }
+    res.send(results)
+});
+
+router.post('/join/:id', auth.jwt, auth.validateGame, (req, res) => {
+    if (gameStates.hasOwnProperty(req.params.id)) {
+        gameStates[req.params.id].players++;
+        gameStates[req.params.id][req.body.uid] = {
+            updated: false
+        };
+    } else {
+        gameStates[req.params.id] = {
+            active: true,
+            players: 1
+        };
+        gameStates[req.params.id][req.body.uid] = {
+            updated: false
+        };
+
+    }
+    res.status(200).send({
+        message: 'success'
+    });
 });
 
 module.exports = router;
